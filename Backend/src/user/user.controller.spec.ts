4c15a1088e6c3e3e646ee1912df444e5a4779154
sherin858 <sherin.reda15@gmail.com>
2023-07-18 12:08:21 +0300

    user-controller tested

--- Backend/src/user/user.controller.spec.ts
+++ Backend/src/user/user.controller.spec.ts
@@ -3,9 +3,14 @@ import { UserController} from './user.controller';
 import { UserService } from './user.service';
 describe('UserController', () => {
   let controller: UserController;
-
   const mockUserService={
-    registerUser:jest.fn().mockImplementationOnce((name:string,email:string,password:string)=>Promise.resolve('123'))
+    registerUser:jest.fn().mockImplementation((name:string,email:string,password:string)=>Promise.resolve('123')),
+    validateUser:jest.fn().mockImplementation((email:string,password:string)=>{
+      if(email=="sherin.reda15@gmail.com" && password=="1234"){
+        return '11111111111'
+      }
+      return undefined
+    })
   }
 
   beforeEach(async () => {
@@ -29,4 +34,15 @@ describe('UserController', () => {
       id:expect.any(String)
     })
   });
+  it('should return token',async()=>{
+    let result=await controller.loginUser("sherin.reda15@gmail.com","1234");
+    expect(result).toEqual(expect.any(String));
+    expect(mockUserService.validateUser).toHaveBeenCalledWith('sherin.reda15@gmail.com', '1234');
+  });
+
+  it('should return undefined',async()=>{
+    let result=await controller.loginUser("rewan@yahoo.com","1234");
+    expect(result).toEqual(undefined);
+    expect(mockUserService.validateUser).toHaveBeenCalledWith("rewan@yahoo.com","1234");
+  })
 });
